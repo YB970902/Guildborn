91c23e12d95c6049a2a51a8e8a4750d27b256223
YB970902 <78996787+YB970902@users.noreply.github.com>
2025-07-03 00:09:13 +0900

    BoCharacter - 기본 정보 추가

--- Assets/Scripts/Bo/Character/BoCharacter.cs
+++ Assets/Scripts/Bo/Character/BoCharacter.cs
@@ -6,6 +6,13 @@ namespace Bo
 {
 	public class BoCharacter
 	{
+		/// <summary> 캐릭터를 구분할 수 있는 고유 인덱스 </summary>
+		public long CharacterIdx { get; private set; }
+		/// <summary> 캐릭터 소유자의 아이디 </summary>
+		public int OwnerID { get; private set; }
+		/// <summary> 캐릭터의 스텟 </summary>
 		public BoStatus Status { get; private set; }
+		/// <summary> 캐릭터의 현재 체력 </summary>
+		public int CurrentHealth { get; private set; }
 	}
 }
